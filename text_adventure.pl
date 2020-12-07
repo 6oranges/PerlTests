31fa26b7f93c30fc167115091e1807cb914e5e2a
Timothy Stander <20standtimt@washk12.org>
2020-12-07 14:15:14 -0700

    map

--- text_adventure.pl
+++ text_adventure.pl
@@ -3,19 +3,58 @@
 use strict;
 use warnings;
 use diagnostics;
-
-my %north =  ( England => 'France',
-	           France => 'England', 
-    	           Spain => 'France', 
-    	           China => 'New York', 
-	           Germany => 'Germany');
+use feature 'say';
+my %north =  ( Start => 'Dog',
+	           Dog => 'DangerFish',
+    	       Treat => 'Place', 
+	           Place => 'Bridge',
+               Bridge => 'Path',
+               Path => 'Corridor',
+               Corridor=>'Corridor',
+               Note=>'Note',
+               Gold=>'Gold',
+               OtherRoom=>'OtherRoom'
+               );
+my %east =  ( Start => 'Treat',
+	           Dog => 'Place',
+	           Place => 'Cliff',
+               Treat => 'Cliff',
+               Bridge => 'Cliff',
+               Note =>'Path',
+               Path=>'Cliff',
+               Gold=>'Corridor',
+               Corridor=>'OtherRoom',
+               OtherRoom=>'OtherRoom'
+               );
+my %west =  ( Start => 'Start',
+	           Dog => 'Dog',
+	           Place => 'Dog',
+               Treat => 'Start',
+               Bridge => 'DangerFish',
+               Path =>'Note',
+               Note=>'Note',
+               Gold=>'Gold',
+               Corridor=>'Gold',
+               OtherRoom=>'Corridor'
+               );
+my %south =  ( Start => 'Start',
+	           Dog => 'Start',
+	           Place => 'Treat',
+               Treat => 'Treat',
+               Bridge => 'Place',
+               Path =>'Bridge',
+               Note=>'DangerFish',
+               Gold=>'Gold',
+               Corridor=>'Path',
+               OtherRoom=>'Corridor'
+               );
 # taking input from user
 my $input = <STDIN>;
 # remove trailing newline
 chomp $input;
 # a subroutine; paramaters are accessed through a global variable @_
 sub Forward {
-   $count = 0; # initial value
+   my $count = 0; # initial value
 
    print "Value of counter is $count\n";
    $count++;
